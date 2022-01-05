a8410f0ff060b314fa45e53ee46f2e098f2931cc
STLeee <a8365885@hotmail.com>
2022-01-05 17:11:59 +0800

    list bugs

--- src/phase_1/frontend/src/components/BugManagement.vue
+++ src/phase_1/frontend/src/components/BugManagement.vue
@@ -1,40 +1,127 @@
 <template>
   <v-container>
-    <v-row class="text-center">
+    <v-row>
       <v-col>
-        <v-row>
-        </v-row>
+        <v-card outlined>
+          <v-card-title>
+            <span class="text-h5 font-weight-black">Bugs</span>
+
+            <v-spacer></v-spacer>
+
+            <v-btn
+              icon
+              large
+              color="black"
+              @click="getBugs"
+              :loading="loading"
+            >
+              <v-icon>mdi-refresh</v-icon>
+            </v-btn>
+          </v-card-title>
+
+          <v-card outlined class="mx-4">
+            <v-data-table
+              :items-per-page="10"
+              :expanded.sync="bugExpanded"
+              show-expand
+              sort-by="id"
+              :headers="bugHeaders"
+              :items="bugs"
+            >
+              <template v-slot:[`item.status`]="{ item }">
+                <v-chip
+                  class="text-uppercase"
+                  :color="getBugStatusColor(item)"
+                  dark
+                >
+                  {{ item.status }}
+                </v-chip>
+              </template>
+              <template v-slot:expanded-item="{ headers, item }">
+                <td :colspan="headers.length">
+                  {{ item.description }}
+                </td>
+              </template>
+            </v-data-table>
+          </v-card>
+        </v-card>
       </v-col>
+    </v-row>
+    <v-row v-for="(bug, i) in bugs" :key="i">
     </v-row>
   </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'BugManagement',
-    data: () => ({
-      apiServer: '127.0.0.1',
-      apiPort: 3000,
-    }),
-
-    methods: {
-      getBugs() {
-        return this.$axios.get(`http://${this.apiServer}:${this.apiPort}/api/bug`).then((res) => {
-          if (res.data.status == 200) return res.data.result
-          else return new Promise.reject(res.data)
-        })
-      }
-    },
-    mounted() {
-      this.getBugs()
+import { mapState } from 'vuex'
+export default {
+  name: 'BugManagement',
+  computed: {
+    ...mapState([
+      'stuffType',
+      'apiIP',
+      'apiPort'
+    ])
+  },
+  data: () => ({
+    loading: false,
+    bugExpanded: [],
+    bugHeaders: [
+      {
+        text: 'Status',
+        value: 'status'
+      },
+      {
+        text: 'ID',
+        value: 'id',
+        align: 'start'
+      },
+      {
+        text: 'Summary',
+        value: 'summary',
+        align: 'start',
+        sortable: false
+      },
+      {
+        text: '',
+        value: 'actions',
+        align: 'end',
+        sortable: false
+      },
+      {
+        text: '',
+        value: 'data-table-expand'
+      },
+    ],
+    bugs: []
+  }),
+  methods: {
+    getBugs() {
+      this.loading = true
+      return this.$axios.get(`http://${this.apiIP}:${this.apiPort}/api/bug`).then((res) => {
+        console.log('res', res)
+        if (res.data.status == 200) return res.data.result
+        else alert(`[${res.data.status}] ${res.data.error}`)
+      })
       .then(bugs => {
-        console.log('bugs', bugs)
+        this.bugs = bugs
+        this.loading = false
       })
       .catch(err => {
-        if (err.status) {
-          alert(`[${err.status}] ${err.error}`)
-        }
+        console.error(err)
+        this.loading = false
       })
     },
-  }
+    getBugStatusColor(bug) {
+      if (bug.status == 'Opened') {
+        return 'green'
+      } else {
+        return 'gray'
+      }
+    }
+  },
+  mounted() {
+    this.getBugs()
+  },
+}
 </script>
