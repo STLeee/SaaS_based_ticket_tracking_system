86b22c5faaa6c0e64dd092a03d568dd54b5aba98
STLeee <a8365885@hotmail.com>
2022-01-05 17:34:58 +0800

    bug action buttons

--- src/phase_1/frontend/src/components/BugManagement.vue
+++ src/phase_1/frontend/src/components/BugManagement.vue
@@ -7,16 +7,22 @@
             <span class="text-h5 font-weight-black">Bugs</span>
 
             <v-spacer></v-spacer>
-
-            <v-btn
-              icon
-              large
-              color="black"
-              @click="getBugs"
-              :loading="loading"
-            >
-              <v-icon>mdi-refresh</v-icon>
-            </v-btn>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  icon
+                  large
+                  color="black"
+                  @click="getBugs"
+                  :loading="loading"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  <v-icon>mdi-refresh</v-icon>
+                </v-btn>
+              </template>
+              <span>Refresh</span>
+            </v-tooltip>
           </v-card-title>
 
           <v-card outlined class="mx-4">
@@ -36,6 +42,50 @@
                 >
                   {{ item.status }}
                 </v-chip>
+              </template>
+              <template v-slot:[`item.actions`]="{ item }">
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-icon
+                      v-if="item.status=='Opened'"
+                      class="mr-4"
+                      color="green"
+                      @click="resolveBug(item)"
+                      v-bind="attrs"
+                      v-on="on"
+                    >
+                      mdi-bug-check
+                    </v-icon>
+                  </template>
+                  <span>Resolve</span>
+                </v-tooltip>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-icon
+                      class="mr-4"
+                      color="black"
+                      @click="editBug(item)"
+                      v-bind="attrs"
+                      v-on="on"
+                    >
+                      mdi-pencil
+                    </v-icon>
+                  </template>
+                  <span>Edit</span>
+                </v-tooltip>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-icon
+                      color="red"
+                      @click="deleteBug(item)"
+                      v-bind="attrs"
+                      v-on="on"
+                    >
+                      mdi-delete
+                    </v-icon>
+                  </template>
+                  <span>Delete</span>
+                </v-tooltip>
               </template>
               <template v-slot:expanded-item="{ headers, item }">
                 <td :colspan="headers.length">
@@ -99,7 +149,6 @@ export default {
     getBugs() {
       this.loading = true
       return this.$axios.get(`http://${this.apiIP}:${this.apiPort}/api/bug`).then((res) => {
-        console.log('res', res)
         if (res.data.status == 200) return res.data.result
         else alert(`[${res.data.status}] ${res.data.error}`)
       })
@@ -118,6 +167,15 @@ export default {
       } else {
         return 'gray'
       }
+    },
+    editBug(bug) {
+      console.log('edit bug', bug)
+    },
+    deleteBug(bug) {
+      console.log('delete bug', bug)
+    },
+    resolveBug(bug) {
+      console.log('resolve bug', bug)
     }
   },
   mounted() {
