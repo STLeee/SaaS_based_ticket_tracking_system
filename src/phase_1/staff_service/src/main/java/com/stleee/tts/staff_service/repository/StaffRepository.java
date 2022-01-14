8fbffb69e3ee54eac4bf761642df24e124c8b6c6
STLeee <a8365885@hotmail.com>
2022-01-14 11:28:09 +0800

    fix useless package

--- src/phase_1/staff_service/src/main/java/com/stleee/tts/staff_service/repository/StaffRepository.java
+++ src/phase_1/staff_service/src/main/java/com/stleee/tts/staff_service/repository/StaffRepository.java
@@ -1,7 +1,5 @@
 package com.stleee.tts.staff_service.repository;
 
-import java.util.List;
-
 import com.stleee.tts.staff_service.model.Staff;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
